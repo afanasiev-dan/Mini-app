28b53aa85cdd02a9c9c8eccaaa7287023f54a0c5
Daniil <arankart@gmail.com>
2025-11-07 00:18:49 +0300

    Контроллер для обменника

--- backend/TelegramMiniAppBackend/models/OrderBuyForm.cs
+++ backend/TelegramMiniAppBackend/models/OrderBuyForm.cs
@@ -3,6 +3,7 @@ namespace TelegramMiniAppBackend.Models;
 public class OrderBuyForm
 {
     public long UserId { get; set; }
+    public string Username { get; set; } = string.Empty;
     public string Currency { get; set; } = "USDT";
     public string Bank { get; set; } = "";
     public decimal Amount { get; set; }
